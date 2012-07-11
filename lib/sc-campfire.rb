126a754c5aec29214a96873b9125d5c37dcff668
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2012-07-11 10:58:16 +0200

    add railtie to supply rake with our additional task

--- lib/sc-campfire.rb
+++ lib/sc-campfire.rb
@@ -1,7 +1,12 @@
-require "sc-campfire/version"
+require 'sc-campfire/campfire'
+require 'sc-campfire/room'
 
 module Sc
   module Campfire
-    # Your code goes here...
+    class Railtie < Rails::Railtie
+      rake_tasks do
+        Dir[File.join(File.dirname(__FILE__),'tasks/*.rake')].each { |f| load f }
+      end
+    end
   end
 end
